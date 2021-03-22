72ec63e63603bda2a5f1fc21a56d41dbdf8f61ce
keeks5456 <Akikogreen08@gmail.com>
2021-03-22 14:00:56 -0700

    user controller semi complete but is rendering, working on journal controller

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,2 +1,17 @@
 class JournalsController < ApplicationController
+  
+  def index 
+    journals = Journal.all
+    render json: journals
+  end
+
+  def show
+    journal = Journal.find_by(params [:id => true])
+    render json: journal
+  end 
+
+  def create 
+    journal = Journal.create(journal_params)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+
+  def create
+    user = User.create(user_params)
+    render json: {user: UserSerializer.new(user)}, status: :created
+  end
+
+  def index 
+    users = User.all
+    render json: user
+  end
+
+  def show 
+    user = User.find_by(params[:id => true])
+    if user
+      render json: user 
+    else
+      render json: { message: 'This ID does not exist' }
+    end
+  end
+
+  def destroy
+    user = User.find_by(params[:id])
+    user.destroy
+
+    render json: { message: 'deleted' }
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
