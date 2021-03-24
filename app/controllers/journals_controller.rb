39d516cf918e3afcfac08921ef7fe7356bf11363
keeks5456 <Akikogreen08@gmail.com>
2021-03-24 16:19:54 -0700

    journal and tracker controllers show, index, create, destroy

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -12,6 +12,18 @@ class JournalsController < ApplicationController
 
   def create 
     journal = Journal.create(journal_params)
+    render json: {journal: JournalSerializer.new(journal)}, status: :created
+  end
+
+  def destroy
+    journal = Journal.find_by(params[:id])
+    journal.destroy
+    render json: {message: 'deleted'}
+  end
+
+  private
+  def journal_params
+    journal.require(:journal).permit(:content, :user_id)
   end
 
 end
--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -1,2 +1,27 @@
 class TrackersController < ApplicationController
+
+  def index 
+    trackers = Tracker.all 
+    render json: trackers 
+  end
+
+  def show
+    tracker = Tracker.find_by(params [:id => true])
+    if tracker 
+      render json: tracker 
+    else 
+      render json: {message: "This ID doesnt exist"}
+    end
+  end
+
+  def create
+    tracker = Tracker.create(tracker_params)
+    render json: {tracker: TrackerSerializer.new(tracker)}, status: :created
+  end
+
+  def destroy
+    tracker = Tracker.find_by(params[:id])
+    tracker.destroy
+    render json: {message: 'deleted'}
+  end
 end
